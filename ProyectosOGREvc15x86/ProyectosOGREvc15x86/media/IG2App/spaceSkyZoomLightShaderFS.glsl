862937a580ee5b52015293e6499253467a3d55d2
VerXxuxZ <125389780+JulianSerranoChacon@users.noreply.github.com>
2024-12-02 16:20:09 +0100

    yo ya, shader del cielo, falta el agua

--- ProyectosOGREvc15x86/ProyectosOGREvc15x86/media/IG2App/spaceSkyZoomLightShaderFS.glsl
+++ ProyectosOGREvc15x86/ProyectosOGREvc15x86/media/IG2App/spaceSkyZoomLightShaderFS.glsl
@@ -3,19 +3,16 @@ uniform sampler2D texturaL;  // Tipo sampler2D para texturas 2D
 uniform sampler2D texturaM;  // -> unidades de textura (int) 
 uniform float BF;  // Blending factor 
 uniform float intLuzAmb; // Luz ambiente blanca 
-float sintime_0_2pi;
 in vec2 vUv0; // Out (del vertex shader) 
+in float zf;
 out vec4 fFragColor; // Out (del fragment shader) 
 void main() 
 { 
-    //vec3 colorL = vec3(texture(texturaL, vUv0)); // Acceso a téxel 
-    //vec3 colorM = vec3(texture(texturaM, vUv0)); // Configuración! 
-    float ZF = sintime_0_2pi * 0.3 + 1.0;
-    vUv0.s = (vUv0.s- 0.5) * (ZF) + 0.5; 
-    vUv0.t = (vUv0.t- 0.5) * (ZF) + 0.5;
+    vec3 colorL = vec3(texture(texturaL, vUv0)); // Acceso a téxel 
+    vec3 colorM = vec3(texture(texturaM, vUv0)); // Configuración! 
     colorL = vec3(texture(texturaL, vUv0)); // Acceso a téxel 
     colorM = vec3(texture(texturaM, vUv0)); // Configuración! 
-    vec3 color = mix(colorL, colorM, BF) * intLuzAmb; //Mix-> (1-BF).colorL + BF.colorM fFragColor = vec4(color, 1.0); 
+    vec3 color = mix(colorL, colorM, BF) * zf * intLuzAmb; //Mix-> (1-BF).colorL + BF.colorM fFragColor = vec4(color, 1.0); 
     fFragColor=vec4(color,1.0);
     
 }
